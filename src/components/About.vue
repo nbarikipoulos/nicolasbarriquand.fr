1f19b99e9ff02f3260fa7373538f88228e876225
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-01-14 00:36:13 +0100

    About section refactored

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,10 +2,26 @@
   section(:id="data.nav['anchor']")
     div(class="section-heading")
       h1(class="title is-2") {{ data.title }}
-      p(class="subtitle is-5") {{ data.subtitle }}
+      //- p(class="subtitle is-5") {{ data.subtitle }}
+
+    //- div(class="container subsection")
+    //-   p(class="has-text-justified") {{ data.intro }}
 
     div(class="container subsection")
-      p(class="has-text-justified") {{ data.intro }}
+      div(class="columns")
+        //- FIXME to factorize
+        div(class="column is-half has-text-justified")
+          p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ok']['title'] }}
+          ul(class="fa-ul")
+            li(v-for="item in data['skill.text']['ok']['items']")
+              b-icon( class="fa-li" pack="fa" icon="check" type="is-success")
+              p {{ item }}
+        div(class="column is-half has-text-justified")
+          p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ko']['title'] }}
+          ul(class="fa-ul")
+            li(v-for="item in data['skill.text']['ko']['items']")
+              b-icon( class="fa-li" pack="fa" icon="times" type="is-danger")
+              p {{ item }}
 
     div(class="container subsection")
       div(class="columns")
@@ -44,25 +60,9 @@
                   ) {{ data['figures']['client']['value'] }}
                   span(
                     class="is-size-6 has-text-weight-bold"
-                  ) {{ data['figures']['client']['label'] }}
+                  )  {{ data['figures']['client']['label'] }}
                     div(class="image is-16by9")
                       img(src="@/assets/pig.gif")
-
-    div(class="container subsection")
-      div(class="columns")
-        //- FIXME to factorize
-        div(class="column is-half has-text-justified")
-          p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ok']['title'] }}
-          ul(class="fa-ul")
-            li(v-for="item in data['skill.text']['ok']['items']")
-              b-icon( class="fa-li" pack="fa" icon="check" type="is-success")
-              p {{ item }}
-        div(class="column is-half has-text-justified")
-          p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ko']['title'] }}
-          ul(class="fa-ul")
-            li(v-for="item in data['skill.text']['ko']['items']")
-              b-icon( class="fa-li" pack="fa" icon="times" type="is-danger")
-              p {{ item }}
 </template>
 <script>
 export default {
