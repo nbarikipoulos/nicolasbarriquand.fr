9ad7ab86d96af6a4ef0308092454eb0e5df252b6
nbarikipoulos <nicolas.barriquand@outlook.fr>
2020-01-16 20:41:25 +0100

    Replace Columns by tiles

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,25 +1,24 @@
 <template lang="pug">
-  section(class='section' :id="data.nav['anchor']")
+  section(class="section" :id="data.nav['anchor']")
     div(class="section-heading")
       h1(class="title is-2") {{ data.title }}
       //- p(class="subtitle is-5") {{ data.subtitle }}
 
     div(class="container subsection")
-      div(class="box")
-        p(class="has-text-justified") {{ data.intro }}
+      p(class="has-text-centered") {{ data.intro }}
 
     div(class="container subsection")
-      div(class="columns")
+      div(class="tile is-ancestor")
         //- FIXME to factorize
-        div(class="column is-half has-text-justified")
-          div(class="box")
+        div(class="tile is-parent is-half has-text-justified")
+          div(class="tile is-child box")
             p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ok']['title'] }}
             ul(class="fa-ul")
               li(v-for="item in data['skill.text']['ok']['items']")
                 b-icon( class="fa-li" pack="fa" icon="check" type="is-success")
                 p {{ item }}
-        div(class="column is-half has-text-justified")
-          div(class="box")
+        div(class="tile is-parent is-half has-text-justified")
+          div(class="tile is-child box")
             p(class="has-text-centered subtitle is-5") {{ data['skill.text']['ko']['title'] }}
             ul(class="fa-ul")
               li(v-for="item in data['skill.text']['ko']['items']")
@@ -27,9 +26,9 @@
                 p {{ item }}
 
     div(class="container subsection")
-      div(class="columns")
-        div(class="column is-half")
-          div(class="box")
+      div(class="tile is-ancestor")
+        div(class="tile is-parent is-half")
+          div(class="tile is-child box")
             p(class="has-text-centered subtitle is-5") {{ data.text['skill.title'] }}
               div(v-for="item in data['skill.bars']")
                 p {{ item.name }}
@@ -38,28 +37,28 @@
                   :max=100
                   size="is-medium"
                 )
-        div(class="column is-half")
-          div(class="box")
+        div(class="tile is-parent is-half")
+          div(class="tile is-child box")
             //- FIXME to factorize
             p(class="has-text-centered subtitle is-5") {{ data.text['figure.title'] }}
-              div(class="columns")
-                div(class="column is-5")
-                  div(class="box")
+              div(class="tile is-ancestor")
+                div(class="tile is-parent is-vertical is-5")
+                  div(class="tile is-child box no-border")
                     span(
                       class="is-size-4 has-text-weight-bold has-text-primary"
                     ) {{ data['figures']['xp']['value'] }}
                     span(
                       class="is-size-6 has-text-weight-bold"
                     )  {{ data['figures']['xp']['label'] }}
-                  div(class="box")
+                  div(class="tile is-child box")
                     span(
                       class="is-size-4 has-text-weight-bold has-text-primary"
                     ) {{ data['figures']['project']['value'] }}
                     span(
                       class="is-size-6 has-text-weight-bold"
                     )  {{ data['figures']['project']['label'] }}
-                div(class="column is-7")
-                  div(class="box")
+                div(class="tile is-parent is-7")
+                  div(class="tile is-child box")
                     span(
                       class="is-size-4 has-text-weight-bold has-text-primary"
                     ) {{ data['figures']['client']['value'] }}
